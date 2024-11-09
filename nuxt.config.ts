b59b6fe76c7313cb2de668abfce24aa92258c446
Adhil A Backer <124715404+adhilabu@users.noreply.github.com>
2024-11-09 22:16:57 +0530

    fix: :bug:
    Added changes to add google adsense.
    
    Added changes to add google adsense.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   routeRules: {
-    // Homepage pre-rendered at build time
     '/': { prerender: true },
-    // Products page generated on demand, revalidates in background, cached until API response changes
     '/products': { swr: true },
-    // Product page generated on demand, revalidates in background, cached for 1 hour (3600 seconds)
     '/products/**': { swr: 3600 },
-    // Blog posts page generated on demand, revalidates in background, cached on CDN for 1 hour (3600 seconds)
     '/blog': { isr: 3600 },
-    // Blog post page generated on demand once until next deployment, cached on CDN
     '/blog/**': { isr: true },
-    // Admin dashboard renders only on client-side
     '/admin/**': { ssr: false },
-    // Add cors headers on API routes
     '/api/**': { cors: true },
-    // Redirects legacy urls
-    '/old-page': { redirect: '/new-page' }
+    '/old-page': { redirect: '/new-page' },
   },
-  modules: ['@nuxt/ui','@pinia/nuxt', '@nuxthq/studio'],
+  modules: ['@nuxt/ui', '@pinia/nuxt', '@nuxthq/studio'],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   postcss: {
@@ -30,7 +22,18 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       backendApiBaseUrl: process.env.BACKEND_API || 'http://127.0.0.1:8000',
-      taipyApiBaseUrl: process.env.TAIPY_API || 'http://127.0.0.1:5000'
-    }
-  }
+      taipyApiBaseUrl: process.env.TAIPY_API || 'http://127.0.0.1:5000',
+    },
+  },
+  app: {
+    head: {
+      script: [
+        {
+          src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5321510867132090",
+          async: true,
+          crossorigin: "anonymous",
+        },
+      ],
+    },
+  },
 })
